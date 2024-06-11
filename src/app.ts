73398553cebac940a18b7a1cdbc1414cf8cf7e1c
Luciano Figueroa <lumusitech@gmail.com>
2024-06-11 19:13:42 -0300

    feat: add github token verification middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 
 (async () => {
   await main()
@@ -9,11 +10,15 @@ import { GithubController } from './presentation/github/controller';
 async function main() {
   const app = express()
 
-  app.use(express.json())
   // if we want work with xwww-form-urlencoded
   // app.use(express.urlencoded({ extended: true }))
 
+  app.use(express.json())
+
+  app.use(GithubSha256Middleware.verifySignature)
+
   const controller = new GithubController()
+
 
   app.post("/api/github", controller.webhookHandler)
 
