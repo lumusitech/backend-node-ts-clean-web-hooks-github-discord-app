f1ac4e8d57baf397e5c259647b1c039402be9046
Luciano Figueroa <lumusitech@gmail.com>
2024-06-11 17:10:56 -0300

    feat: add github event service

--- src/config/presentation/github/controller.ts
+++ src/config/presentation/github/controller.ts
@@ -1,17 +1,33 @@
 import type { Request, Response } from "express";
+import { GithubService } from '../../../services/github.service';
 
 export class GithubController {
   // dependencies injection on constructor
-  // constructor() {}
+  constructor(private readonly githubService = new GithubService()) { }
 
   webhookHandler = (req: Request, res: Response) => {
     const githubEvent = req.header('x-github-event') ?? 'unknown';
     const signature = req.header('x-hub-signature-256') ?? 'unknown';
+    let message: string
 
     const payload = req.body;
 
-    console.log(JSON.stringify(payload));
+    switch (githubEvent) {
+      case "star":
+        message = this.githubService.onStar(payload);
+        break
 
+      case "issues":
+        message = this.githubService.onIssue(payload);
+        break
+
+      default:
+        message = `unknown event: ${githubEvent}`;
+
+    }
+
+
+    console.log({ message });
 
     res.status(202).send("Accepted");
   }
