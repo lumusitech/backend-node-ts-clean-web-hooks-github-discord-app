b6fa0ced150a25741d7464f0fb7292c1f37729de
Luciano Figueroa <lumusitech@gmail.com>
2024-06-11 13:15:41 -0300

    chore: add env vars

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
+import express from 'express';
+import { envs } from './config';
+
 (async () => {
   await main()
 })()
 
 async function main() {
-  console.log('Hello World!');
+  const app = express()
+
+  app.listen(envs.PORT, () => {
+    console.log('Server running on port', envs.PORT);
+  })
+
 }
